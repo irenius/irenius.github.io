e7ae167dad6b7d012a4e41b081ce37da794c91b5
olle <olle@dorunner.se>
2014-09-21 20:49:18 +0200

    Site basics

---
+++ mail/contact_me.php
@@ -0,0 +1,25 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'olleirenius91@gmail.com';
+$email_subject = "Irenius website form:  $name";
+$email_body = "Somebody filled out the form at irenius.github.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@irenius.github.io";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;
